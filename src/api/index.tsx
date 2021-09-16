9bf7d08a85c29499f6d43d8bfd64922f71983c8f
SimeonAleksov <aleksov_s@outlook.com>
2021-09-16 15:17:28 +0200

    Remove unused packages, add toast to login fail

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,18 +1,29 @@
 import axios from 'axios'
 
-const baseURL = "http://localhost:8000"
+const baseURL = "http://194.195.247.179"
 
 export const bareAPI = axios.create({
-    baseURL: "http://localhost:8000"
+    baseURL: "http://194.195.247.179",
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+        'Origin': 'localhost:3000',
+    }
 })
 
 const api = axios.create({
     baseURL: baseURL,
+    headers: {
+        'Access-Control-Allow-Origin': '*'
+    }
 })
 
 
 export const ghapi = axios.create({
     baseURL: "https://api.github.com",
+    headers: {
+        'Access-Control-Allow-Origin': '*'
+    },
 })
 
 ghapi.interceptors.request.use(
