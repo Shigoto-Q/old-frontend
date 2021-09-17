ad853dcf4224e5e9d001bf2eb1aa60bac88820b8
SimeonAleksov <aleksov_s@outlook.com>
2021-09-17 12:03:41 +0200

    Replace local url

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -4,16 +4,10 @@ const baseURL = "http://shigoto.live"
 
 export const bareAPI = axios.create({
     baseURL: "http://shigoto.live",
-    headers: {
-        'Access-Control-Allow-Origin': '*',
-    }
 })
 
 const api = axios.create({
     baseURL: baseURL,
-    headers: {
-        'Access-Control-Allow-Origin': '*'
-    }
 })
 
 
